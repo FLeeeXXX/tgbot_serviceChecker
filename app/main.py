d2bf624a3b32dbb96eea6cd20bbf57661024044c
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 18:37:00 +0500

    update

--- app/main.py
+++ app/main.py
@@ -12,26 +12,17 @@ from app.handlers.user_handlers import user_handlers
 from app.config import settings
 
 dp = Dispatcher()
-dp.include_router(user_handlers)
 
-async def on_startup(app):
-    await bot.set_webhook(settings.WH_URL)
+dp.include_routers(
+    user_handlers,
+)
 
-async def on_shutdown(app):
-    await bot.delete_webhook()
-    await bot.session.close()
 
-async def handle(request):
-    update = Update(**await request.json())
-    await dp.feed_update(bot, update)
-    return web.Response()
-
-app = web.Application()
-app.on_startup.append(on_startup)
-app.on_shutdown.append(on_shutdown)
-app.router.add_post(settings.WH_PATH, handle)
+# webhook вместо start_polling юзается только на проде
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.set_my_commands(commands=, scope=types.BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=8443)
-
-
+    asyncio.run(main())
