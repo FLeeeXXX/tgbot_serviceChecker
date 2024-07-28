04b98756ac4349b8f54bfd1a57851cab9ca49f02
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 19:50:19 +0500

    upd

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import logging
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -13,10 +12,6 @@ from app.common.common import bot
 from app.handlers.user_handlers import user_handlers
 from app.config import settings
 
-# Настройка логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 WEBHOOK_PATH = "/webhook"
 WEB_SERVER_PORT = 3000
 WEB_SERVER_HOST = "0.0.0.0"
@@ -24,21 +19,11 @@ WEB_SERVER_URL = f"https://tgbot-servicechecker.onrender.com{WEBHOOK_PATH}"
 
 
 async def on_startup(bot: Bot):
-    logger.info(f"Setting webhook to {WEB_SERVER_URL}")
-    webhook_set = await bot.set_webhook(WEB_SERVER_URL)
-    if webhook_set:
-        logger.info("Webhook set successfully")
-    else:
-        logger.error("Failed to set webhook")
+    await bot.set_webhook(WEB_SERVER_URL)
 
 
 async def on_shutdown(bot: Bot):
-    logger.info("Deleting webhook")
-    webhook_deleted = await bot.delete_webhook(drop_pending_updates=True)
-    if webhook_deleted:
-        logger.info("Webhook deleted successfully")
-    else:
-        logger.error("Failed to delete webhook")
+    await bot.delete_webhook(drop_pending_updates=True)
 
 
 async def create_app():
@@ -66,14 +51,12 @@ async def create_app():
 
 
 def main():
-    try:
-        app = asyncio.run(create_app())
-        web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
-    except Exception as e:
-        logger.error(f"An error occurred: {e}")
-    finally:
-        if not app['client_session'].closed:
-            asyncio.run(app['client_session'].close())
+    app = asyncio.run(create_app())
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+
+
+if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
