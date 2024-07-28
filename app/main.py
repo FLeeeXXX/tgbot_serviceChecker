a25b1e40affeed965410dffa24d95ddc5cd7fce9
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 18:01:35 +0500

    upd main.py

--- app/main.py
+++ app/main.py
@@ -5,36 +5,33 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import asyncio
 from aiohttp import web
-from aiogram import Dispatcher
+from aiogram import Bot, Dispatcher
+from aiogram.types import Update
 from app.common.common import bot
 from app.handlers.user_handlers import user_handlers
 from app.config import settings
 
-# Создаем экземпляр Dispatcher
 dp = Dispatcher()
+dp.include_router(user_handlers)
 
-# Включаем маршрутизаторы
-dp.include_routers(
-    user_handlers,
-)
-
-# Функция для инициализации вебхука
 async def on_startup(app):
     await bot.set_webhook(settings.WH_URL)
 
-# Функция для завершения работы
 async def on_shutdown(app):
     await bot.delete_webhook()
     await bot.session.close()
 
-# Создаем приложение aiohttp
+async def handle(request):
+    update = Update(**await request.json())
+    await dp.feed_update(bot, update)
+    return web.Response()
+
 app = web.Application()
 app.on_startup.append(on_startup)
 app.on_shutdown.append(on_shutdown)
+app.router.add_post(settings.WH_PATH, handle)
 
-# Добавляем путь для вебхука
-app.router.add_post(settings.WH_PATH, dp.webhook_handler)
-
-# Запуск приложения
 if __name__ == '__main__':
     web.run_app(app, host='0.0.0.0', port=8443)
+
+
