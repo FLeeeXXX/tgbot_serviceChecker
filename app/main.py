5d2c26ff7dd952f764cc283816a9d4ab9ac1ae51
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 21:05:35 +0500

    try polling

--- app/main.py
+++ app/main.py
@@ -1,54 +1,23 @@
 import sys
 import os
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import asyncio
-from aiohttp import web
 from aiogram import Bot, Dispatcher
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiogram.types import Update
 from app.common.common import bot
 from app.handlers.user_handlers import user_handlers
 from app.config import settings
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-async def on_startup(bot: Bot):
-    await bot.set_webhook(settings.WEB_SERVER_URL)
-
-
-async def on_shutdown(bot: Bot):
-    await bot.delete_webhook(drop_pending_updates=True)
-
-
-async def create_app():
+async def main():
     dp = Dispatcher()
-
     dp.include_router(user_handlers)
 
-    dp.startup.register(on_startup)
-    dp.shutdown.register(on_shutdown)
-
-    app = web.Application()
-
-    webhook_requests_handler = SimpleRequestHandler(
-        dispatcher=dp,
-        bot=bot,
-    )
-
-    webhook_requests_handler.register(app, path=settings.WEBHOOK_PATH)
-
-    setup_application(app, dp, bot=bot)
-
-    return app
-
-
-def main():
-    loop = asyncio.get_event_loop()
-    app = loop.run_until_complete(create_app())
-    web.run_app(app, host=settings.WEB_SERVER_HOST, port=settings.WEB_SERVER_PORT)
-
+    try:
+        await dp.start_polling(bot, allowed_updates=Update.ANY)
+    finally:
+        await dp.storage.close()
+        await bot.session.close()
 
 if __name__ == '__main__':
-    main()
-
+    asyncio.run(main())
