2352cce58c5f9b992e4b0cb50cf3425c97d0c723
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 14:47:01 +0500

    comm

--- migrations/env.py
+++ migrations/env.py
@@ -17,7 +17,7 @@ from app.proxies.models import Proxy
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL.replace('postgresql+asyncpg', 'postgresql')}?async_fallback=True")
+config.set_main_option("sqlalchemy.url", f"{settings.DATABASE_URL}?async_fallback=True")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
