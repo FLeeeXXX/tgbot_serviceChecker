ab4ab22dc8792a129d91055e12270bfcc09f67d5
Danil Shulgin <43463155+FLeeeXXX@users.noreply.github.com>
2024-07-28 18:47:20 +0500

    test comm

--- app/main.py
+++ app/main.py
@@ -6,23 +6,45 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import asyncio
 from aiohttp import web
 from aiogram import Bot, Dispatcher
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from aiogram.types import Update
 from app.common.common import bot
 from app.handlers.user_handlers import user_handlers
 from app.config import settings
 
-dp = Dispatcher()
-
-dp.include_routers(
-    user_handlers,
-)
 
 
-# webhook вместо start_polling юзается только на проде
+WEBHOOK_PATH = "/webhook"
+WEB_SERVER_PORT = 3000
+WEB_SERVER_HOST = "0.0.0.0"
+WEB_SERVER_URL = f"{WEB_SERVER_HOST}:{WEBHOOK_PATH}"
+
+
+async def on_startup(bot: Bot):
+    await bot.set_webhook(WEB_SERVER_URL)
+
+
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    # await bot.set_my_commands(commands=, scope=types.BotCommandScopeAllPrivateChats())
-    await dp.start_polling(bot)
+    dp = Dispatcher()
+
+    dp.include_routers(
+        user_handlers,
+    )
+
+    dp.startup.register(on_startup)
+
+    app = web.Application()
+
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+    )
+
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+
+    setup_application(app, dp, bot=bot)
+
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
 
 if __name__ == '__main__':
     asyncio.run(main())
